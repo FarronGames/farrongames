5402364973207a33607d0ae4a160a14a1613da39
FarronGames <info@farrongames.com>
2023-01-09 20:39:55 +0100

    Deploy

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="backgroundroto-top normal gray"><img src="/img/roto.svg"></div>
+    <div class="footer-contenido">
+        <div class="footer-logo"><img src="/img/logo_farrongames.svg"></div>
+        <div class="footer-menu">
+            <ul>
+                <li><a href="#">Aviso legal</a></li>
+                <li><a href="#">Política de privacidad</a></li>
+                <li><a href="#">Política de cookies</a></li>
+            </ul>
+        </div>
+        <div class="footer-redes">
+            <x-gmdi-facebook />
+            <x-phosphor-instagram-logo />
+        </div>
+        <hr>
+        <div class="footer-copy"><p>FarronGames © 2023.</p><span>Todos los derechos reservados.</span></div>
+    </div>
+</footer>
